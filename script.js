c3aee9da8d2a3ec340ff9fd0e47049b39df449be
polllkikh <106408382+polllkikh@users.noreply.github.com>
2022-10-26 17:04:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let deuses = ['Aegir', 'Aud', 'Balder'];
+console.log(array);
+
+deuses.push('Loki', 'Odin', 'Frey');
+console.log(array);
+
+let numbrer = [1,2,4,5,6,7,8,]
+
+let.log(numbrer)
+numbrer.sort()
+
+numbrer.reverse()
+console.log(numbrer)
